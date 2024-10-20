e0299bf4f1daf2093e541fa7495964c364c4e091
animal-23 <148389990+animal-23@users.noreply.github.com>
2024-10-20 17:33:01 +0530

    Update sub_cipher.py

--- sub_cipher.py
+++ sub_cipher.py
@@ -1,32 +1,26 @@
 import string
 import random
 
-def monoalphabetic_cipher(text, cipher_key=None, decrypt=False):
+def encrypt(text):
     alphabet = list(string.ascii_lowercase)
-    
-    # Generate a cipher key if not provided
-    if cipher_key is None:
-        shuffled_alphabet = alphabet[:]
-        random.shuffle(shuffled_alphabet)
-        cipher_key = dict(zip(alphabet, shuffled_alphabet))
-    else:
-        # If decrypting, reverse the cipher key
-        if decrypt:
-            cipher_key = {v: k for k, v in cipher_key.items()}
-    
-    # Encrypt or decrypt the text
-    translated_text = ''.join(cipher_key.get(char, char) for char in text.lower())
-    
-    return translated_text, cipher_key
+    shuffled_alphabet = alphabet[:]
+    random.shuffle(shuffled_alphabet)
+    cipher_key = dict(zip(alphabet, shuffled_alphabet))
+    encrypted_text = ''.join(cipher_key.get(char, char) for char in text.lower())
+    return encrypted_text, cipher_key
 
-# Example usage
+def decrypt(text, cipher_key):
+    reversed_cipher_key = {v: k for k, v in cipher_key.items()}
+    decrypted_text = ''.join(reversed_cipher_key.get(char, char) for char in text.lower())
+    return decrypted_text
+
+
 plaintext = "hello world"
-
-# Encrypt the plaintext
-encrypted_text, cipher_key = monoalphabetic_cipher(plaintext)
-
-# Decrypt the encrypted text
-decrypted_text, _ = monoalphabetic_cipher(encrypted_text, cipher_key, decrypt=True)
+encrypted_text, cipher_key = encrypt(plaintext)
+decrypted_text = decrypt(encrypted_text, cipher_key)
+print("Plaintext:", plaintext)
+print("Encrypted:",encrypted_text)
+print("Decrypted:",decrypted_text)
 
 #print(f"Cipher Key: {cipher_key}")
 print(f"Plaintext: {plaintext}")
